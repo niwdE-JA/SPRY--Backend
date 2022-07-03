5b0bb94311d385edca125124f147020114a0edad
A.J-Edwin <dezwhy97@gmail.com>
2022-07-04 00:51:44 +0100

    Temporary allow all origins

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use( bodyParser.json() );
 app.use( sessions_config );
 
 app.use( (req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000' );
+    res.setHeader('Access-Control-Allow-Origin', '*' );
     res.setHeader('Access-Control-Allow-Credentials', 'true' );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type' );
     next();
